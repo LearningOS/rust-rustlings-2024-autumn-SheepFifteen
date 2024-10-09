53e3cd44a8312d83f370cbb86b6f8fad21ff9ab3
hjalanzy <1750651409@qq.com>
2024-10-09 22:36:13 +0800

    finished structs

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Package {
     sender_country: String,
@@ -29,12 +27,15 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> f64 {
         // Something goes here...
+        self.weight_in_grams as f64 * cents_per_gram as f64 / 100.0
+        // as f64 类型转换
     }
 }
 

4bdd920e497382acf201d24df99a2774793f7309
hjalanzy <1750651409@qq.com>
2024-10-09 23:40:06 +0800

    correct modules

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
@@ -12,7 +10,7 @@ mod sausage_factory {
     }
 
     pub fn make_sausage() {
-        let _ = get_secret_recipe();
+        get_secret_recipe();
         println!("sausage!");
     }
 }
